0026811d60e0e670c3a689496f18baf6ca73d008
TheChukwuma <egwuonwuchukwuma74@gmail.com>
2022-06-30 19:16:09 +0100

    "Bro code tutorial"

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+     var food = new String[3];
+     String[] car = {"Benz", "Camry"};
+     food[0] = "rice";
+     food[1] = "beans";
+     food[2] = "egg";
+
+     System.out.println(car[0]);
+     System.out.println(food[1]);
+
+    }
+
+
+}
+
